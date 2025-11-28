95de4ed78fecccd76d061f0208afd9e7364971f0
Spray <marcosbandeira@live.com>
2025-11-28 18:58:37 -0300

    Create orderRoutes.js

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+// Definição das rotas conforme URLs solicitadas [cite: 228, 229, 231]
+router.post('/order', orderController.createOrder);
+router.get('/order/list', orderController.listOrders); // A rota específica vem antes da genérica (:id)
+router.get('/order/:id', orderController.getOrder);
+router.put('/order/:id', orderController.updateOrder);
+router.delete('/order/:id', orderController.deleteOrder);
+
+module.exports = router;
